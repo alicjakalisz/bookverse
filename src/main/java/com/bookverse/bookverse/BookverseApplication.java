a3a86e6877558066c4a6a8af62ccd9fcebcb7176
Alicja <alicja.barbara.kalisz@gmail.com>
2025-07-02 11:41:15 +0200

    running dockerized mysql locally

--- src/main/java/com/bookverse/bookverse/BookverseApplication.java
+++ src/main/java/com/bookverse/bookverse/BookverseApplication.java
@@ -2,8 +2,10 @@ package com.bookverse.bookverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.bookverse.bookverse.model")
 public class BookverseApplication {
 
 	public static void main(String[] args) {
